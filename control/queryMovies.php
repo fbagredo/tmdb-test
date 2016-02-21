6f6c872c9e3f897064438a0fd9e4b1ef06cd7f65
fbagredo <fernando.becerra.a@gmail.com>
2016-02-21 12:04:26 -0500

    adjusment

--- control/queryMovies.php
+++ control/queryMovies.php
@@ -29,7 +29,7 @@ foreach (array_keys($results) as $key){
 			$personArray =
 			array ($profilePhoto,
 				$results[$key]['name'],
-				'<a href="../tmdb-test/view/viewMoviesByActor.php?personId='.$results[$key]['id'].'">List of movies</a>'
+				'<a href="../view/viewMoviesByActor.php?personId='.$results[$key]['id'].'">List of movies</a>'
 		);
 		array_push($peopleArray, $personArray);
 }
